679f4ec54d155f48c5424924a1c0cc6447eb05bb
Balamurugan S <132822490+BALAMURUGAN5363@users.noreply.github.com>
2025-05-12 15:57:03 +0530

    Add files via upload

---
+++ src/app/services/data.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+  private apiUrl = 'http://localhost:5001/api';
+
+  constructor(private http: HttpClient) {}
+
+  uploadFile(file: File): Observable<any> {
+    const formData = new FormData();
+    formData.append('file', file);
+    return this.http.post(`${this.apiUrl}/upload`, formData);
+  }
+
+  getAnalysis(): Observable<any> {
+    return this.http.get(`${this.apiUrl}/analysis`);
+  }
+}
